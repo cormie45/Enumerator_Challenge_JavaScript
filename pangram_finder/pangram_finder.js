55b85e523e1b1ccc1d745f13f740f6b0ae7aa0a1
Steven Cormack <stevec@Stevens-MBP.lan>
2021-05-05 17:51:20 +0100

    7 in 1 baby

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase;
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+};
 
 PangramFinder.prototype.isPangram = function () {
-
-}
+    let string = this.phrase.toLowerCase();
+    return this.alphabet.every(letter => string.includes(letter));
+};
 
 module.exports = PangramFinder;
--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,9 +1,10 @@
 const UpperCaser = function (words) {
-    this.words = words
-}
+    this.words = words;
+};
+
 UpperCaser.prototype.toUpperCase = function () {
     const toUpper = this.words.map(word => word.toUpperCase());
     return toUpper;
-}
+};
 
 module.exports = UpperCaser;
