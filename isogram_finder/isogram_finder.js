79891e4f62e0f9e3c914fcd4b35e4ab1d4914afe
Steven Cormack <stevec@Stevens-MBP.lan>
2021-05-05 20:28:50 +0100

    possible changes

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,9 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return this.word.every((letter, index) => this.word.indexOf(letter) === index);
 }
 
 module.exports = IsogramFinder;
