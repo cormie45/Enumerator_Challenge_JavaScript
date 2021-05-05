0716ce8025d1241501f0e26eb4cc9281d1d13fd3
Steven Cormack <stevec@Stevens-MBP.lan>
2021-05-05 20:31:46 +0100

    extension complete

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,10 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
+    this.sortedWord = word.toLowerCase().split('').sort().join('');
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    return otherWords.filter(word => word.toLowerCase().split('').sort().join('') === this.sortedWord && word !== this.word);
 }
 
 module.exports = AnagramFinder;
